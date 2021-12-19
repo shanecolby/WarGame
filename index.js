c89c70c4d47856b3b55a6f69ef9f7d7179a9d5e7
shanecolby <shanecolbyzero8@gmail.com>
2021-12-19 16:28:44 -0600

    display remaining when newdeck is clicked and have draw btn disabled once remaining is 0

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 let deckId
+const remainingText = document.getElementById("remaining")
+const drawCardBtn = document.getElementById("drawCard-btn")
+const newDeckBtn = document.getElementById("newDeck-btn")
 
-document.getElementById("newDeck-btn").addEventListener("click", function () {
+newDeckBtn.addEventListener("click", function () {
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
     .then(res => res.json())
     .then(data => {
       deckId = data.deck_id
       console.log(data)
       console.log(deckId)
+      remainingText.textContent = `Remaining: ${data.remaining}`
+
     })
 })
 
 
 
-document.getElementById("drawCard-btn").addEventListener("click", function () {
+drawCardBtn.addEventListener("click", function () {
   fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
     .then(res => res.json())
     .then(data => {
@@ -22,7 +27,12 @@ document.getElementById("drawCard-btn").addEventListener("click", function () {
       document.getElementById("cardContainer").children[1].innerHTML = `
         <img src=${data.cards[1].image} class="card"/>
       `
+      remainingText.textContent = `Remaining: ${data.remaining}`
       console.log(data)
+
+      if (data.remaining === 0) {
+        drawCardBtn.disabled = true
+      }
     })
 })
 
