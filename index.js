d5438f88618315fe02327ad2b07f7e0d89b10716
shanecolby <shanecolbyzero8@gmail.com>
2021-12-19 15:47:11 -0600

    outline html and display cards after initial draw

---
+++ index.js
@@ -0,0 +1,27 @@
+let deckId
+
+document.getElementById("newDeck-btn").addEventListener("click", function () {
+  fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
+    .then(res => res.json())
+    .then(data => {
+      deckId = data.deck_id
+      console.log(data)
+      console.log(deckId)
+    })
+})
+
+
+
+document.getElementById("drawCard-btn").addEventListener("click", function () {
+  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("cardContainer").children[0].innerHTML = `
+        <img src=${data.cards[0].image} />
+      `
+      document.getElementById("cardContainer").children[1].innerHTML = `
+        <img src=${data.cards[1].image} />
+      `
+      console.log(data)
+    })
+})
